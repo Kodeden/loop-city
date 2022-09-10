61014a811a6faa5336623ec9fae69c16e585fe8f
rpyoungdale <youngdale95@gmail.com>
2022-09-10 12:17:33 -0500

    loop-city-challenge

---
+++ index.js
@@ -0,0 +1,24 @@
+const citizens = ['Ryan', 'Kolton', 'Kody']; // Add more names to grow Loop City!
+
+/////////////////////// DON'T ALTER THIS CODE //////////////////////////
+const populationCount = document.getElementById('populationCount');
+const citizenList = document.getElementById('citizenList');
+const addCitizenBtn = document.getElementById('addCitizenBtn');
+populationCount.innerText = citizens.length;
+////////////////////////////////////////////////////////////////////////
+
+// HINT!!! Looks like the way to get the amount of names is citizens.length
+// Maybe that could be used in our loop.....just a thought
+
+
+/*
+  CODE TO PUT INSIDE OF LOOP BLOCK
+
+  const listElement = document.createElement('li');
+  listElement.innerText = citizens[i];
+  citizenList.appendChild(listElement);
+*/
+
+/////////////////////// WRITE YOUR CODE HERE /////////////////////////
+
+//////////////////////////////////////////////////////////////////////
